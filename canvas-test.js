cc37d995e045368f9336fe17a9023aa209d28679
Ian Dalton <iain.dalton@gmail.com>
2012-04-01 05:53:24 -0600

    restore lost commits

--- canvas-test.js
+++ canvas-test.js
@@ -1,19 +1,20 @@
-var NUM_BALLS      =  5
-var BALL_DIAMETER  = 16
-var BALL_SPEED     =  2
-var BALL_COLOR     = "#592f2d"
-var BALL_SPRITE    = Image();
-var GUN_LENGTH     = 44
-var GUN_WIDTH      = 14
-var GUN_COLOR      = "#9dacb2"
-var GUN_SPRITE_V   = Image();
-var GUN_SPRITE_H   = Image();
-var BARRIER_WIDTH  = GUN_WIDTH - 4
-var TICK_LENGTH_MS = 10
-BALL_SPRITE.src    = 'images/spinning-globe.png'
-GUN_SPRITE_H.src   = 'images/gun-horiz.png'
-GUN_SPRITE_V.src   = 'images/gun-vert.png'
+var NUM_BALLS      =  5;
+var BALL_DIAMETER  = 16;
+var BALL_SPEED     =  2;
+var BALL_COLOR     = "#592f2d";
+var BALL_SPRITE    = new Image();
+BALL_SPRITE.src    = 'images/spinning-globe.png';
+var GUN_LENGTH     = 44;
+var GUN_WIDTH      = 14;
+var GUN_COLOR      = "#9dacb2";
+var GUN_SPRITE_H   = new Image();
+GUN_SPRITE_H.src   = 'images/gun-horiz.png';
+var GUN_SPRITE_V   = new Image();
+GUN_SPRITE_V.src   = 'images/gun-vert.png';
+var BARRIER_WIDTH  = GUN_WIDTH - 4;
+var TICK_LENGTH_MS = 10;
 
+var CANVAS;
 var CTX;
 var WIDTH;
 var HEIGHT;
@@ -21,11 +22,10 @@ var MIN_X;
 var MIN_Y;
 var MAX_X;
 var MAX_Y;
-var BALLS = [];
-var FILLED_AREAS = [];
-var GUN;
-var BARRIER;
+var WORLD;
 var TICK = 0;
+var DRAW_INTERVAL;
+var PAUSED = false;
 
 /**
  * Restrict b to the range [a, c].
@@ -47,6 +47,11 @@ function cartesian_to_polar(x, y) {
 function polar_to_cartesian(magnitude, angle) {
     return {x: magnitude * Math.cos(angle),
             y: magnitude * Math.sin(angle)};
+}
+
+/* Choose an element from an Array at random */
+function random_elt(choices) {
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function Ball(x, y, radius, speed, heading, color, img) {
@@ -88,17 +93,20 @@ Ball.prototype.handle_wall_collisions = function () {
         y = this.y,
         radius = this.radius;
 
-    if (x + radius > WIDTH || x - radius < 0)
+    if (x + radius > WIDTH || x - radius < 0) {
         this.dx = -this.dx;
-    if (y + radius > HEIGHT || y - radius < 0)
+    }
+    if (y + radius > HEIGHT || y - radius < 0) {
         this.dy = -this.dy;
+    }
 };
 
 Ball.prototype.handle_ball_collisions = function (other) {
     var dist_x, dist_y, radii;
 
-    if (this === other)
+    if (this === other) {
         return;
+    }
     dist_x = other.x - this.x;
     dist_y = other.y - this.y;
     radii = this.radius + other.radius;
@@ -129,9 +137,13 @@ Ball.prototype.handle_ball_collisions = function (other) {
 Ball.prototype.collide = Ball.prototype.handle_ball_collisions;
 
 function le() {
-    for (var i = 1; i < arguments.length; i++)
-        if (!(arguments[i - 1] <= arguments[i]))
+    var i;
+
+    for (i = 1; i < arguments.length; i++) {
+        if (arguments[i - 1] > arguments[i]) {
             return false;
+        }
+    }
     return true;
 }
 
@@ -179,20 +191,28 @@ Ball.prototype.draw = function () {
             this.y - img.frame_size / 2,
             img.frame_size,
             img.frame_size);
-        if (TICK % 10 == 0) {
+        if (TICK % 10 === 0) {
             img.frame = (img.frame + 1) % img.num_frames;
         }
     }
 };
 
-function Gun(x, y, horizontal_p, color) {
+Ball.prototype.draw_wireframe = function () {
+    CTX.fillStyle = "blue";
+    CTX.beginPath();
+    CTX.arc(this.x, this.y, 1, 0, 2 * Math.PI, true);
+    CTX.closePath();
+    CTX.fill();
+};
+
+function Gun(x, y, horizontal_p, color, use_sprite) {
     this.x = x;
     this.y = y;
     this.horizontal_p = horizontal_p;
     this.w = horizontal_p ? GUN_LENGTH : GUN_WIDTH;
     this.h = horizontal_p ? GUN_WIDTH : GUN_LENGTH;
     this.color = color || "#000";
-    this.use_sprite = true;
+    this.use_sprite = use_sprite || true;
     this.sprite_h = GUN_SPRITE_H;
     this.sprite_v = GUN_SPRITE_V;
 }
@@ -220,23 +240,55 @@ Gun.prototype.draw = function () {
 function update_gun_pos(evt) {
   if (   evt.pageX > MIN_X && evt.pageX < MAX_X
       && evt.pageY > MIN_Y && evt.pageY < MAX_Y) {
-      GUN.x = evt.pageX - MIN_X;
-      GUN.y = evt.pageY - MIN_Y;
+      WORLD.gun.x = evt.pageX - MIN_X;
+      WORLD.gun.y = evt.pageY - MIN_Y;
   }
 }
 
 /* Flip gun on space bar press */
 function flip_gun(evt) {
     var tmp;
-    if (evt.keyCode == 32) {
-        GUN.horizontal_p = !GUN.horizontal_p;
-        tmp = GUN.w;
-        GUN.w = GUN.h;
-        GUN.h = tmp;
+    if (evt.keyCode === 32) {
+        WORLD.gun.horizontal_p = !WORLD.gun.horizontal_p;
+        tmp = WORLD.gun.w;
+        WORLD.gun.w = WORLD.gun.h;
+        WORLD.gun.h = tmp;
     }
 }
 
-function Barrier(x, y, horizontal_p, speed, color) {
+function FilledArea(x1, x2, y1, y2, color) {
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+    this.color = color || "#000";
+}
+
+FilledArea.prototype.draw = function () {
+    CTX.fillStyle = this.color;
+    CTX.beginPath();
+    CTX.fillRect(this.x1, this.y1, this.x2 - this.x1, this.y2 - this.y1);
+    CTX.closePath();
+    CTX.fill();
+};
+
+function in_filled_area(x, y) {
+    var i, fa;
+
+    if (x < 0 || x > WIDTH || y < 0 || y > HEIGHT) {
+        return true;
+    }
+    for (i = 0; i < WORLD.filled_areas.length; i++) {
+        fa = WORLD.filled_areas[i];
+        if (x > fa.x1 && x < fa.x2 && y > fa.y1 && y < fa.y2) {
+            return fa;
+        }
+    }
+    return false;
+}
+
+function Barrier(world, x, y, horizontal_p, speed, color) {
+    this.world = world;
     this.x1 = x;
     this.x2 = x;
     this.y1 = y;
@@ -254,10 +306,10 @@ function Barrier(x, y, horizontal_p, speed, color) {
 }
 
 Barrier.prototype.save = function () {
-    FILLED_AREAS.push(
+    this.world.filled_areas.push(
         new FilledArea(this.x1, this.x2, this.y1, this.y2));
-    BARRIER = null;
-}
+    this.world.barrier = null;
+};
 
 Barrier.prototype.move = function () {
     var end1 = in_filled_area(this.x1, this.y1),
@@ -284,87 +336,38 @@ Barrier.prototype.draw = function () {
 function create_barrier(evt) {
   if (   evt.pageX > MIN_X && evt.pageX < MAX_X
       && evt.pageY > MIN_Y && evt.pageY < MAX_Y) {
-      if (!BARRIER)
-          BARRIER = new Barrier(evt.pageX - MIN_X, evt.pageY - MIN_Y,
-                                GUN.horizontal_p, BALL_SPEED);
+      if (!WORLD.barrier) {
+          WORLD.barrier = new Barrier(WORLD,
+                                      evt.pageX - MIN_X, evt.pageY - MIN_Y,
+                                      WORLD.gun.horizontal_p, BALL_SPEED);
+      }
   }
 }
 
-function FilledArea(x1, x2, y1, y2, color) {
-    this.x1 = x1;
-    this.x2 = x2;
-    this.y1 = y1;
-    this.y2 = y2;
-    this.color = color || "#000";
+function World(balls, filled_areas, gun, barrier) {
+    this.balls = balls;
+    this.filled_areas = filled_areas;
+    this.gun = gun;
+    this.barrier = barrier;
 }
 
-FilledArea.prototype.draw = function () {
-    CTX.fillStyle = this.color;
-    CTX.beginPath();
-    CTX.fillRect(this.x1, this.y1, this.x2 - this.x1, this.y2 - this.y1);
-    CTX.closePath();
-    CTX.fill();
+World.prototype.clone = function () {
+    function clone_array(arr) {
+        return arr.map(function(obj) {return Object.create(obj);});
+    }
+    var w = new World(clone_array(this.balls),
+                      clone_array(this.filled_areas),
+                      Object.create(this.gun),
+                      this.barrier ? Object.create(this.barrier) : undefined);
+    if (w.barrier) {
+        w.barrier.world = w;
+    }
+    return w;
 };
 
-function in_filled_area(x, y) {
-    var i, fa;
+World.prototype.move = function () {
+    var i, j, new_world, ball1, ball2, fa;
 
-    if (x < 0 || x > WIDTH || y < 0 || y > HEIGHT)
-        return true;
-    for (i = 0; i < FILLED_AREAS.length; i++) {
-        fa = FILLED_AREAS[i];
-        if (x > fa.x1 && x < fa.x2 && y > fa.y1 && y < fa.y2)
-            return fa;
-    }
-    return false;
-}
-
-function move(balls) {
-    for (var i = 0; i < balls.length; i++) {
-        balls[i].x += balls[i].dx;
-        balls[i].y += balls[i].dy;
-    }
-}
-
-function collide(balls, others) {
-    var i, j, ball, x1, x2, y1, y2;
-
-    if (others === undefined) {
-        for (i = 0; i < balls.length; i++)
-            for (j = i + 1; j < balls.length; j++)
-                balls[i].collide(balls[j]);
-    } else {
-        for (i = 0; i < balls.length; i++) {
-            ball = balls[i];
-            for (j = 0; j < others.length; j++) {
-                o = others[j];
-                x1 = ball.x - ball.radius;
-                x2 = ball.x + ball.radius;
-                y1 = ball.y - ball.radius;
-                y2 = ball.y + ball.radius;
-                // If ball is hitting the top or bottom of the other
-                if ((le(o.y1, y1, o.y2) || le(o.y1, y2, o.y2))
-                    && ball.radius >= o.x1 && ball.radius <= o.x2) {
-                    ball.dy = -ball.dy;
-                }
-                // If ball is hitting a side of the other
-                if ((le(o.x1, x1, o.x2) || le(o.x1, x2, o.x2))
-                    && ball.radius >= o.y1 && ball.radius <= o.y2) {
-                    ball.dx = -ball.dx;
-                }
-            }
-        }
-    }
-}
-
-function clear() {
-    CTX.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
-function draw() {
-    var i, j, ball1, ball2, fa;
-
-    clear();
     /*
       (move balls)
       (collide balls walls)
@@ -372,67 +375,144 @@ function draw() {
       (collide balls barrier)
       (collide balls)
      */
-    for (i = 0; i < BALLS.length; i++) {
-        ball1 = BALLS[i];
+    for (i = 0; i < this.balls.length; i++) {
+        ball1 = this.balls[i];
         ball1.move();
         ball1.handle_wall_collisions();
-        if (fa = in_filled_area(ball1.x, ball1.y))
+        fa = in_filled_area(ball1.x, ball1.y);
+        if (fa) {
             ball1.handle_filled_collisions(fa);
-        if (BARRIER)
-            ball1.handle_filled_collisions(BARRIER);
+        }
+        if (this.barrier) {
+            ball1.handle_filled_collisions(this.barrier);
+        }
     }
-    for (i = 0; i < BALLS.length; i++) {
-        ball1 = BALLS[i];
-        for (j = i + 1; j < BALLS.length; j++) {
-            ball2 = BALLS[j];
+    for (i = 0; i < this.balls.length; i++) {
+        ball1 = this.balls[i];
+        for (j = i + 1; j < this.balls.length; j++) {
+            ball2 = this.balls[j];
             ball1.handle_ball_collisions(ball2);
         }
-        ball1.draw();
     }
-    if (BARRIER) BARRIER.move();
-    if (BARRIER) BARRIER.draw();
-    for (i = 0; i < FILLED_AREAS.length; i++) {
-        FILLED_AREAS[i].draw();
+    if (this.barrier) {
+        this.barrier.move();
     }
-    GUN.draw();
     ++TICK;
-}
+};
 
-/* Choose an element from an Array at random */
-function random_elt(choices) {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+World.prototype.draw = function () {
+    var i, ball;
 
-function init() {
-    var canvas = $("#canvas")[0],
-        i;
+    CTX.clearRect(0, 0, WIDTH, HEIGHT);
+    for (i = 0; i < this.balls.length; i++) {
+        ball = this.balls[i];
+        ball.draw();
+    }
+    if (this.barrier) {
+        this.barrier.draw();
+    }
+    for (i = 0; i < this.filled_areas.length; i++) {
+        this.filled_areas[i].draw();
+    }
+    this.gun.draw();
+    if (PAUSED) {
+        for (i = 0; i < this.future.length; i++) {
+            this.future[i].draw_wireframe();
+        }
+    }
+};
 
-    $(document).mousemove(update_gun_pos);
-    $(document).mousedown(create_barrier);
-    $(document).keydown(flip_gun);
-    CTX = canvas.getContext("2d");
-    WIDTH = canvas.width;
-    HEIGHT = canvas.height;
-    MIN_X = $("#canvas").offset().left;
-    MIN_Y = $("#canvas").offset().top;
-    MAX_X = MIN_X + WIDTH;
-    MAX_Y = MIN_Y + WIDTH;
+World.prototype.draw_wireframe = function () {
+    this.balls.forEach(function(ball) {ball.draw_wireframe();});
+};
+
+World.prototype.move_and_draw = function() {
+    var new_world, i;
+
+    this.move();
+    this.draw();
+
+    if (!this.future) {
+        this.future = [];
+        new_world = this.clone();
+        for (i = 0; i < 100; i++) {
+            new_world.move();
+            this.future.push(new_world);
+            new_world = new_world.clone();
+        }
+    }
+    this.future.shift();
+    new_world = this.future[this.future.length - 1].clone();
+    new_world.move();
+    this.future.push(new_world);
+};
+
+function init_world() {
+    var balls = [];
+    var gun;
+    var filled_areas;
+    var i;
+
     for (i = 0; i < NUM_BALLS; i++) {
-        BALLS.push(new Ball(
+        balls.push(new Ball(
             Math.random() * WIDTH, Math.random() * HEIGHT, BALL_DIAMETER / 2,
             BALL_SPEED, 2 * Math.PI * Math.random(), BALL_COLOR, BALL_SPRITE));
     }
-    GUN = new Gun(WIDTH / 2, HEIGHT / 2, true, GUN_COLOR);
-    FILLED_AREAS =
+    gun = new Gun(WIDTH / 2, HEIGHT / 2, true, GUN_COLOR);
+    filled_areas =
         [new FilledArea(-10, 0, -10, HEIGHT + 10),
          new FilledArea(WIDTH, WIDTH + 10, -10, HEIGHT + 10),
          new FilledArea(WIDTH - 10, WIDTH + 10, -10, 0),
          new FilledArea(WIDTH - 10, WIDTH + 10, HEIGHT, HEIGHT + 10)];
-    setInterval(FPS, 1000);
-    return setInterval(draw, TICK_LENGTH_MS);
+    WORLD = new World(balls, filled_areas, gun);
+}
+
+function toggle_pause() {
+    if (PAUSED) {
+        PAUSED = false;
+        DRAW_INTERVAL = setInterval(WORLD.move_and_draw.bind(WORLD),
+                                    TICK_LENGTH_MS);
+        CANVAS.style.cursor = 'none';
+    } else {
+        PAUSED = true;
+        clearInterval(DRAW_INTERVAL);
+        WORLD.draw();
+        CANVAS.style.cursor = 'auto';
+    }
+}
+
+function init_buttons() {
+    $("#play_pause").click(toggle_pause);
+    $("#step").click(WORLD.move_and_draw.bind(WORLD));
+    $("#reset").click(function () {
+        init_world();
+        this.blur();
+    });
 }
 
 function FPS() {
     $('#tick').html(TICK + " FPS");
     TICK = 0;
+}
+
+function init() {
+    $(document).mousemove(update_gun_pos);
+    $(document).mousedown(create_barrier);
+    $(document).keydown(flip_gun);
+    CANVAS = $("#canvas")[0];
+    CTX = CANVAS.getContext("2d");
+    WIDTH = CANVAS.width;
+    HEIGHT = CANVAS.height;
+    MIN_X = $("#canvas").offset().left;
+    MIN_Y = $("#canvas").offset().top;
+    MAX_X = MIN_X + WIDTH;
+    MAX_Y = MIN_Y + WIDTH;
+    init_world();
+    init_buttons();
+    setInterval(FPS, 1000);
+    if (DRAW_INTERVAL) {
+        clearInterval(DRAW_INTERVAL);
+    }
+    DRAW_INTERVAL = setInterval(WORLD.move_and_draw.bind(WORLD),
+                                TICK_LENGTH_MS);
 }
